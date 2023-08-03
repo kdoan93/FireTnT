c244f727350c6f61212b2f021d63b599b33e3757
Kenny Doan <kdoan93@gmail.com>
2023-08-03 19:19:12 -0400

    Validation Middleware completed

---
+++ backend/utils/validation.js
@@ -0,0 +1,26 @@
+const { validatoinResult } = require('express-validator');
+
+//  Middleware formatting errors from express-validator middleware
+//  handleValidationErrors calls validationResult from express-validator package
+//  passing in the req. If !validationErrors returned from validationResult() next(err)
+//  If validation errors exist, create error w/ validation error messages 
+const handleValidationErrors = (req, _res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+        const errors = {};
+        validationErrors.array().forEach(error => errors[error.path] = error.msg);
+
+        const err = Error("Bad request.");
+        err.errors = errors;
+        err.status = 400;
+        err.title = "Bad request.";
+        next(err);
+    }
+    next();
+}
+
+
+
+
+module.exports = { handleValidationErrors };
