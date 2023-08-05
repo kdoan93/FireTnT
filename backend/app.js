2900283e742b7eefc7eb5424f81b732812c0e6ab
Kenny Doan <kdoan93@gmail.com>
2023-08-05 14:15:58 -0400

    coded out error-handlers

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// require('express-async-errors');
+require('express-async-errors');
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
@@ -47,46 +47,46 @@ app.use( csurf({
 
 
 
-/***        Error-Handlers      ***/
-//  Catch unhandled requests and foward to error handler.
-app.use((_req, _res, next) => {
-  const err = new Error('The requested resource couldn\'t be found.');
-  err.title = 'Resource Not Found';
-  err.errors = { message: "The requested resource couldn't be found." };
-  err.status = 404;
-  next(err);
-})
+// /***        Error-Handlers      ***/
+// //  Catch unhandled requests and foward to error handler.
+// app.use((_req, _res, next) => {
+//   const err = new Error('The requested resource couldn\'t be found.');
+//   err.title = 'Resource Not Found';
+//   err.errors = { message: "The requested resource couldn't be found." };
+//   err.status = 404;
+//   next(err);
+// })
 
-//  2nd error handler for catching Sequelize errors & formatting before sending error response
+// //  2nd error handler for catching Sequelize errors & formatting before sending error response
 
-// Process sequelize errors
-app.use((err, _req, _res, next) => {
-  // check if error is a Sequelize error:
-  if (err instanceof ValidationError) {
-    let errors = {};
-    for (let error of err.errors) {
-      errors[error.path] = error.message;
-    }
-    err.title = 'Validation error';
-    err.errors = errors;
-  }
-  next(err);
-});
+// // Process sequelize errors
+// app.use((err, _req, _res, next) => {
+//   // check if error is a Sequelize error:
+//   if (err instanceof ValidationError) {
+//     let errors = {};
+//     for (let error of err.errors) {
+//       errors[error.path] = error.message;
+//     }
+//     err.title = 'Validation error';
+//     err.errors = errors;
+//   }
+//   next(err);
+// });
 
-//  Error handler for formatting all errors before returning a res.JSON.
-//  Will include error message as a JSON object with key-value pairs & error stack trace with status code of error message
-// Error formatter
-app.use((err, _req, res, _next) => {
-  res.status(err.status || 500);
-  console.error(err);
-  res.json({
-    title: err.title || 'Server Error',
-    message: err.message,
-    errors: err.errors,
-    stack: isProduction ? null : err.stack
-  });
-});
-/***        Error-Handlers      ***/
+// //  Error handler for formatting all errors before returning a res.JSON.
+// //  Will include error message as a JSON object with key-value pairs & error stack trace with status code of error message
+// // Error formatter
+// app.use((err, _req, res, _next) => {
+//   res.status(err.status || 500);
+//   console.error(err);
+//   res.json({
+//     title: err.title || 'Server Error',
+//     message: err.message,
+//     errors: err.errors,
+//     stack: isProduction ? null : err.stack
+//   });
+// });
+// /***        Error-Handlers      ***/
 
 //  Connects all the routes
 app.use(routes);
