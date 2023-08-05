dca7f52d2a7a7519fd5d974a9f677c96de0f69cb
Kenny Doan <kdoan93@gmail.com>
2023-08-04 23:12:44 -0400

    adjusted app.use(routes) in backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -49,8 +49,6 @@ app.use(
   );
 /***    SECURITY MIDDLEWARE  ***/
 
-//  Connects all the routes
-app.use(routes);
 
 
 /***        Error-Handlers      ***/
@@ -61,38 +59,40 @@ app.use((_req, _res, next) => {
     err.errors = { message: "The requested resource couldn't be found." };
     err.status = 404;
     next(err);
-})
+  })
 
-//  2nd error handler for catching Sequelize errors & formatting before sending error response
-const { ValidationError } = require('sequelize');
+  //  2nd error handler for catching Sequelize errors & formatting before sending error response
+  const { ValidationError } = require('sequelize');
 
-app.use((err, _req, _res, next) => {
+  app.use((err, _req, _res, next) => {
     //  check if error is a Sequelize error
     if (err instanceof ValidationError) {
         let errors = {};
         for (let error of err.errors) {
             errors[error.path] = error.message;
-        }
+          }
         err.title = 'Validation error';
         err.errors = errors;
-    }
-    next(err);
+      }
+      next(err);
 });
 
 //  Error handler for formatting all errors before returning a res.JSON.
 //  Will include error message as a JSON object with key-value pairs & error stack trace with status code of error message
 app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
+  res.status(err.status || 500);
     console.error(err);
     res.json({
         title: err.title || 'Server Error',
         message: err.message,
         errors: err.errors,
         stack: isProduction ? null : err.stack
-    });
+      });
 });
 /***        Error-Handlers      ***/
 
+//  Connects all the routes
+app.use(routes);
 
 
 //  Export app.js
