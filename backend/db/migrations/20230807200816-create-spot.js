95fc4c32206b5b642dd60a9a328606090b90e1db
Kenny Doan <kdoan93@gmail.com>
2023-08-08 18:47:02 -0400

    Reverse changes from last commit

--- backend/db/migrations/20230807200816-create-spot.js
+++ backend/db/migrations/20230807200816-create-spot.js
@@ -16,7 +16,7 @@ module.exports = {
       },
       ownerId: {
         type: Sequelize.INTEGER,
-        references: { model: 'User', key: 'id' }
+        references: { model: 'Users', key: 'id' }
       },
       address: {
         type: Sequelize.STRING,
