206addba5113fb5a8015596d07e9f95d0c2e6a7a
Kenny Doan <kdoan93@gmail.com>
2023-08-09 18:29:54 -0400

    Create a review and Delete a review IN REVIEW

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -5,7 +5,32 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { Review, Spots, User, ReviewImages } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
+
+
+router.delete('/:reviewId', requireAuth, async (req, res) => {
+    const review = await Review.findByPk(req.params.reviewId)
+
+    //  If review doesn't exist, throw error
+    if(!review) return res.status(404).json({ message: "Review couldn't be found" })
+
+    //  Check if current userId is same as Reviews.userId
+    if(review.userId === req.user.id) {
+        await review.destroy()
+        return res.status(200).json({ message: "Successfully deleted!"})
+    } else res.status(403).json({ message: "Forbidden" })
+
+    //  Finds review by reviewId
+    // const review = await Review.findByPk(req.params.reviewId)
+
+
+    // //  Confirm review is created by current user before allowing to delete review
+    // //  Delete review and return deletion success message
+    // const usersReview = await Review.findOne({ where: { userId: req.user.id } })
+    // if (usersReview) await review.destroy()
+    // return res.status(200).json({ message: "Successfully deleted" })
+})
 
 
 
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Op } = require('sequelize');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-const { Spot, User, Rating, SpotImage } = require('../../db/models');
+const { Spot, User, Rating, SpotImage, Review } = require('../../db/models');
 const { setTokenCookie } = require('../../utils/auth');
 // const bcrypt = require('bcryptjs');
 const { requireAuth } = require('../../utils/auth');
@@ -23,6 +23,15 @@ const validateSpot = [
     handleValidationErrors
 ];
 
+
+//  Validate review
+const validateReview = [
+    check('review').exists({ checkFalsy: true }).withMessage('Review text is required'),
+    check('stars').exists({ checkFalsy: true }).isInt({ min: 1, max: 5 }).withMessage('Stars must be an integer from 1 to 5'),
+    handleValidationErrors
+];
+
+
 //  Create new spot
 router.post('/', requireAuth, validateSpot, async (req, res) => {
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
@@ -33,6 +42,7 @@ router.post('/', requireAuth, validateSpot, async (req, res) => {
     });
     return res.status(201).json( newSpot )
 })
+
 
 //  Get all spots
 router.get('/', async (req, res) => {
@@ -45,6 +55,26 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const spots = await Spot.findByPk(req.params.id)
     return res.status(200).json( spots )
+})
+
+
+//  Router to create a review
+router.post('/:spotId/reviews', requireAuth, validateReview, async(req, res) => {
+    const { review, stars } = req.body;
+
+    //  Check if spot exists
+    const spotExists = await Spot.findByPk(req.params.spotId)
+    if (!spotExists) return res.status(404).json({ message: "Spot couldn't be found" })
+
+    //  Check if user has made review on select spot yet
+    const hasReviewed = await Review.findOne({ where: { spotId: req.params.spotId, userId: req.user.id } })
+    if (hasReviewed) return res.status(500).json({ message: "User already has a review for this spot" });
+
+    //  Creating a new review
+    const newReview = await Review.create({
+        userId: req.user.id, spotId: parseInt(req.params.spotId), review, stars
+    });
+    return res.status(201).json(newReview)
 })
 
 
