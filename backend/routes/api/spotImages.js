c0607a48c57584e885c0b29ea358c5add3ecf6a3
Kenny Doan <kdoan93@gmail.com>
2023-08-10 22:19:41 -0400

    Adjusted code for testing spot-images

--- backend/routes/api/spotImages.js
+++ backend/routes/api/spotImages.js
@@ -6,13 +6,14 @@ const { requireAuth } = require('../../utils/auth');
 const { SpotImage, Spot } = require('../../db/models');
 const router = express.Router();
 
-//  Get all spot-images *for testing*
+/***        Get all spot-images ***for testing***       ***/
 router.get('/', async (req, res) => {
-    const images = await SpotImage.findAll({include: { model: Spot }})
+    const images = await SpotImage.findAll({include: { model: Spot, attributes: ['ownerId'] }})
     return res.json(images)
 })
 
 
+/***        Delete a SpotImage      ***/
 router.delete('/:imageId', requireAuth, async (req, res) => {
     const spotImage = await SpotImage.findByPk(req.params.imageId, {
         include: { model: Spot }
@@ -20,7 +21,7 @@ router.delete('/:imageId', requireAuth, async (req, res) => {
 
     //  If not spotImage found, return error message
     if (!spotImage) return res.status(404).json({ message: "Spot Image couldn't be found" });
-    
+
     if (spotImage.Spot.ownerId === req.user.id) {
         await spotImage.destroy()
         return res.status(200).json({ message: "Successfully deleted" })
