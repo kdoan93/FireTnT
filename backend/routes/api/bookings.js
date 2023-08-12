e0119bd045f94f5eb0a5681f582569b5284a1ebe
Kenny Doan <kdoan93@gmail.com>
2023-08-11 21:46:28 -0400

    EOD. Started on get all bookings for current user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const { Op } = require('sequelize');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { User, Spots } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+const { User, Spot, Review, Booking } = require('../../db/models');
 const router = express.Router();
+
+
+/***        Get all of the Current User's Bookings      ***/
+router.get('/current', requireAuth, async (req, res) => {
+    const bookings = await Booking.findAll({
+        where: {userId: req.user.id},
+        include: [
+            { model: Spot, attributes: [ 'id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price' ] }]
+    })
+
+    return res.status(200).json({Bookings: bookings})
+})
 
 
 
