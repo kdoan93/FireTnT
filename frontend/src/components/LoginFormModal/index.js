c7b1de344a0b944179340eb92216a001bfd6a411
Kenny Doan <kdoan93@gmail.com>
2023-08-28 13:57:04 -0400

    Added Demo User Login

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
+import { NavLink } from "react-router-dom";
 import "./LoginForm.css";
 
 function LoginFormModal() {
@@ -25,41 +26,51 @@ function LoginFormModal() {
       });
   };
 
+  const demoLogin = (e) => {
+    // e.preventDefault();
+    return dispatch(sessionActions.login({ credential: "john.smith", password: "secret password" }))
+    .then(closeModal)
+  }
+
   return (
     <div className="LoginFormModal">
       <h1>Log In</h1>
       <form onSubmit={handleSubmit}>
         <div className="loginModalInputsButton">
-        <ul>
-          {errors.message && <p>{errors.message}</p>}
-          <label>
-            <input
-              type="text"
-              placeholder="Username or Email"
-              value={credential}
-              onChange={(e) => setCredential(e.target.value)}
-              required
-            />
-          </label>
-        </ul>
-        <ul>
-          <label>
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-        </ul>
-        <button
-          type="submit"
-          disabled={credential.length < 4 || password.length < 6}
-        >Log In</button>
-
+          <ul>
+            {errors.message && <p>{errors.message}</p>}
+            <label>
+              <input
+                type="text"
+                placeholder="Username or Email"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+              />
+            </label>
+          </ul>
+          <ul>
+            <label>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+          </ul>
+          <button
+            type="submit"
+            disabled={credential.length < 4 || password.length < 6}
+          >
+            Log In
+          </button>
         </div>
       </form>
+      <NavLink exact to='/' className='demoUser' onClick={(e) => demoLogin()}>
+        Demo User
+      </NavLink>
     </div>
   );
 }
