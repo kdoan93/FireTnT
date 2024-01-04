e61750da605d09441d2d446fc648a6d555b0a34d
Kenny Doan <kdoan93@gmail.com>
2024-01-04 17:29:45 -0500

    Added more seed data for spots and spot images

--- backend/db/seeders/20230808155351-demo-spot.js
+++ backend/db/seeders/20230808155351-demo-spot.js
@@ -14,7 +14,7 @@ module.exports = {
     await Spot.bulkCreate([
       {
         ownerId: 1,
-        address:"1 Disney Lane",
+        address: "1 Disney Lane",
         city: "Greenville",
         state: "South Carolina",
         country: "USA",
@@ -26,7 +26,7 @@ module.exports = {
       },
       {
         ownerId: 2,
-        address:"2 Universal Lane",
+        address: "2 Universal Lane",
         city: "Blue Ridge",
         state: "Georgia",
         country: "USA",
@@ -38,7 +38,7 @@ module.exports = {
       },
       {
         ownerId: 3,
-        address:"3 Main Street",
+        address: "3 Main Street",
         city: "Bolton",
         state: "New York",
         country: "USA",
@@ -50,7 +50,7 @@ module.exports = {
       },
       {
         ownerId: 4,
-        address:"4 Main Street",
+        address: "4 Main Street",
         city: "Breckenridge",
         state: "Colorado",
         country: "USA",
@@ -59,6 +59,102 @@ module.exports = {
         name: "Three Sisters Lookout",
         description: "A FOURTH place where web developers are created",
         price: 724.00
+      },
+      {
+        ownerId: 1,
+        address: "350 Mountain Road",
+        city: "Macon",
+        state: "North Carolina",
+        country: "USA",
+        lat: -41.7645,
+        lng: -22.4730,
+        name: "Forge Lake Lodge",
+        description: "Stone columns rise from rocky shores at this Topton home on Lake Nantahala. Decks with living and dining areas, a barbecue, and a hot tub look out to lake and mountains, and steps lead to the private dock. Timber and stone lend rustic charm in generous rooms; velvet and leather take inspiration from classic hunting lodges. Hiking in Nantahala National Forest is just beyond the property.",
+        price: 1660.00
+      },
+      {
+        ownerId: 2,
+        address: "21 Watershed Way",
+        city: "Bryson City",
+        state: "North Carolina",
+        country: "USA",
+        lat: -40.7622,
+        lng: -2.4220,
+        name: "The Watershed Cabin",
+        description: "Nestled between an abundance of leafy trees, this North Carolina escape boasts complete privacy. The 7,500-square-foot home sits at the edge of a peninsula on Fontana Lake with rippling waters below and soaring mountain peaks above. Grab a bottle of wine to sip on the covered porch — perhaps while soaking in the salt-water hot tub. Billiards and a cinema room keep the entertainment going at home.",
+        price: 3250.00
+      },
+      {
+        ownerId: 3,
+        address: "320 Shore Way",
+        city: "Isle of Palms",
+        state: "South Carolina",
+        country: "USA",
+        lat: -40.2245,
+        lng: -2.2430,
+        name: "Shore Beats Work",
+        description: "Perfect for larger groups, this breezy island home in the Isle of Palms community has accommodations for 12 and plenty of scenic space for everyone to find privacy when needed. From the rooftop balcony, you’ll enjoy sweeping ocean views. For a closer look, follow the beach pathway to the shore for a swim. Later, get some rest in a poolside lounge chair or lay back in the hot tub.",
+        price: 1470.00
+      },
+      {
+        ownerId: 4,
+        address: "119 W 42nd Street",
+        city: "Manhattan",
+        state: "New York",
+        country: "USA",
+        lat: -50.7645,
+        lng: 21.4730,
+        name: "Skyline Views",
+        description: "Dont look further  my amazing apartment has a stunning Manhattan skyline . Quick access to Hell's Kitchen, Hudson Yards, Time Square, Javits Center, Bryant Park, The Vessel and more",
+        price: 318.00
+      },
+      {
+        ownerId: 1,
+        address: "1 Hollywood Drive",
+        city: "Los Angeles",
+        state: "California",
+        country: "USA",
+        lat: 42.7645,
+        lng: 11.4730,
+        name: "Architectural Wonder Above Sunset-WeHo w/ Big View",
+        description: "Midcentury modern 2bed/2bath stilt home with spectacular views above Sunset Strip (2 blocks up from Hollywood + Fairfax). Only blocks from the action, but very private and quiet. Recent renovations from roof to foundation, heat/AC system, 1 Giga/sec wifi, wired in + out with 11 speakers, movie projector + two 4k TVs (free Netflix, HBOMax and AppleTV+), 2-car parking with level 2 electric charger.",
+        price: 345.00
+      },
+      {
+        ownerId: 2,
+        address: "93 Fresh Street",
+        city: "Beverly Hills",
+        state: "California",
+        country: "USA",
+        lat: 40.7245,
+        lng: 12.4730,
+        name: "Chateau de Banks",
+        description: "Just off North Beverly Hills Drive, this classical French style estate is in prime location for enjoying all this iconic area has to offer. With an incredible amount of indoor and outdoor space, it's perfect for large scale entertaining, extended family vacations, and maybe even a romantic wedding ceremony.",
+        price: 16264.00
+      },
+      {
+        ownerId: 3,
+        address: "4 Lake Lane",
+        city: "Zephyr Cove",
+        state: "Nevada",
+        country: "USA",
+        lat: 1.7645,
+        lng: 27.4730,
+        name: "Million Dollar Views",
+        description: "Sierra Sunset Estate enjoys the most ideal location a lakefront home on Lake Tahoe can boast. Less than 10 minutes to Stateline, Nevada, the opportunities for entertainment are endless. If gaming brings you joy, pop into Harrah's Lake Tahoe, Montbleu Resort or Hard Rock Hotel and Casino for an array of gambling options. These establishments also offer a variety of venues for concerts, shows, spa delights and shopping. World class skiing is just up the hill, literally, with Heavenly Ski Resort the proud mountain of 4,800 skiable acres (the most in Lake Tahoe).",
+        price: 28000.00
+      },
+      {
+        ownerId: 4,
+        address: "360 Coast View Drive",
+        city: "Jenner",
+        state: "California",
+        country: "USA",
+        lat: 62.7645,
+        lng: 2.4730,
+        name: "Sonoma Coast•Arch•Estate•Dramatic Ocean VUs",
+        description: "Sonoma Coast is a Frank Lloyd Wright inspired modern California Ranch w/ subtle Prairie & International architectural design; perched on a promontory overlooking cove w/ jaw dropping ocean views and mesmerizing - audible crashing white water waves. Complete with spa, BBQ, dry/wet sauna, chefs kitchen, lux bedding and linens, Le Creuset crockery, Mauviel stainless steel cookware, Viking appliances, Wolf small kitchen appliances, Jura Espresso Machine with frother, WIFI, streaming Roku & Alexa.",
+        price: 1500.00
       },
     ])
   },
--- backend/db/seeders/20230810162913-demo-spot-image.js
+++ backend/db/seeders/20230810162913-demo-spot-image.js
@@ -109,6 +109,206 @@ module.exports = {
         url: 'https://a0.muscache.com/im/pictures/48e844c8-abc8-435a-9caf-a38af6096a84.jpg?im_w=1200',
         preview: false
       },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-910287057304342656/original/44f27bcc-d830-4b4c-9051-18066e6bb72f.jpeg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-910287057304342656/original/5d5cc81c-d004-41a5-a2cb-c95c31570e21.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-910287057304342656/original/70669fef-f621-47ff-b877-1291117f4f1d.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-910287057304342656/original/c9145b9c-de83-46c7-b4fb-5c2ea2a3202a.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-910287057304342656/original/1a3a8902-2d52-4632-8de2-f88308f89a39.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-701384721983228885/original/294c5d86-6ebc-4913-9d6b-c1b971f68f6b.jpeg?im_w=1200',
+        preview: true
+      },
+      {
+      spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-701384721983228885/original/853e61f5-b8ec-49c7-90ca-4991063cc864.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-701384721983228885/original/29d7438a-9a67-4688-b5c9-74d6737b3651.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-701384721983228885/original/fd803838-5978-48ff-8f56-87618341172e.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-701384721983228885/original/bb4bfa71-c5c2-45e6-80d2-dc8e716565d7.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-765886237687379456/original/bd33707d-5b0b-49dd-b38f-0bded9f13925.jpeg?im_w=1200',
+        preview: true
+      },
+      {
+      spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-765886237687379456/original/c54415fe-b205-4ff2-acd3-bfd72707f009.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-765886237687379456/original/77429a34-c52e-4ac5-ba22-cf9056f45fc4.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-765886237687379456/original/62b57771-bb44-401f-9455-0144232ea517.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-765886237687379456/original/0fce550e-876b-4482-9398-b86cf093eba7.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-1008785726031193489/original/a3f77a9d-af1d-4b1e-a740-f8af82a3e72f.jpeg?im_w=1200',
+        preview: true
+      },
+      {
+      spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-1008785726031193489/original/21a4a2b5-165e-489c-8faf-7c0dc4347f4f.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-1008785726031193489/original/a5bb9673-bfaa-4886-86d1-6c9df29dd104.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-1008785726031193489/original/c81c8d4e-566c-4376-b74e-47fcb13b670c.jpeg?im_w=1440',
+        preview: false
+      },
+      {
+      spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-1008785726031193489/original/06e8d56b-fa84-48e1-b1f4-6f8b0c1ff35b.jpeg?im_w=1440',
+        preview: false
+      },
+      {
+      spotId: 9,
+        url: 'https://a0.muscache.com/im/pictures/7b57bf26-bbf6-4ae9-9166-8dcb988c3c7b.jpg?im_w=1200',
+        preview: true
+      },
+      {
+      spotId: 9,
+        url: 'https://a0.muscache.com/im/pictures/838e8be3-be72-454d-a45e-15f7af89d679.jpg?im_w=1440',
+        preview: false
+      },
+      {
+      spotId: 9,
+        url: 'https://a0.muscache.com/im/pictures/c02587fe-f0de-4b83-a7f9-e746205a8e98.jpg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 9,
+        url: 'https://a0.muscache.com/im/pictures/76b8ed0b-7d85-498f-add9-5da056b90e87.jpg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 9,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-37654064/original/056f4b63-f18a-4c98-a9ac-da5b47fab73c.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 10,
+        url: 'https://a0.muscache.com/im/pictures/b4db5900-b90e-4cc3-b12b-6d17953d0079.jpg?im_w=1200',
+        preview: true
+      },
+      {
+      spotId: 10,
+        url: 'https://a0.muscache.com/im/pictures/64986466-734d-4c39-ab52-69f6b9c1059b.jpg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 10,
+        url: 'https://a0.muscache.com/im/pictures/abd1ddb2-33bb-48d8-8000-d10015d52a7c.jpg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 10,
+        url: 'https://a0.muscache.com/im/pictures/76f43fbe-2524-4469-8eb3-ab12ac0738e0.jpg?im_w=1440',
+        preview: false
+      },
+      {
+      spotId: 10,
+        url: 'https://a0.muscache.com/im/pictures/6c3932d1-484c-4953-b1ba-d4074f2e40e6.jpg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 11,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-47336678/original/fae24879-7e22-43b8-b186-3212346ed35b.jpeg?im_w=1200',
+        preview: true
+      },
+      {
+      spotId: 11,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-47336678/original/5d19214e-0e5a-40de-8457-b5e2a4aa3271.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 11,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-47336678/original/91280dc8-9780-4a02-9f86-8c8cf9bb8268.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 11,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-47336678/original/f99282f1-f24e-490e-8261-018ba8a11027.jpeg?im_w=1440',
+        preview: false
+      },
+      {
+      spotId: 11,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-47336678/original/1aaaca5c-b1a3-4f5c-9bda-bbbe47c648d8.jpeg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 12,
+        url: 'https://a0.muscache.com/im/pictures/c4c92198-fb3a-4c4b-bbb6-3aa8af8f7e73.jpg?im_w=1200',
+        preview: true
+      },
+      {
+      spotId: 12,
+        url: 'https://a0.muscache.com/im/pictures/76672df0-df76-490b-8f77-dee11757ceae.jpg?im_w=1440',
+        preview: false
+      },
+      {
+      spotId: 12,
+        url: 'https://a0.muscache.com/im/pictures/63ee70fc-4a43-4fb8-9bd9-8f9c72cc8dfe.jpg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 12,
+        url: 'https://a0.muscache.com/im/pictures/f81a7ee0-ea83-436d-9ed6-71d53aaae7a6.jpg?im_w=1200',
+        preview: false
+      },
+      {
+      spotId: 12,
+        url: 'https://a0.muscache.com/im/pictures/f502e121-683a-4011-9ef8-ddf5ddba6256.jpg?im_w=1200',
+        preview: false
+      },
     ])
   },
 
