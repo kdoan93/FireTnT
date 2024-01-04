8e5a2af10101e6f9d221931c06177ec095a41375
Kenny Doan <kdoan93@gmail.com>
2024-01-04 18:00:03 -0500

    Lowered spot description lengths

--- backend/db/seeders/20230808155351-demo-spot.js
+++ backend/db/seeders/20230808155351-demo-spot.js
@@ -69,7 +69,7 @@ module.exports = {
         lat: -41.7645,
         lng: -22.4730,
         name: "Forge Lake Lodge",
-        description: "Stone columns rise from rocky shores at this Topton home on Lake Nantahala. Decks with living and dining areas, a barbecue, and a hot tub look out to lake and mountains, and steps lead to the private dock. Timber and stone lend rustic charm in generous rooms; velvet and leather take inspiration from classic hunting lodges. Hiking in Nantahala National Forest is just beyond the property.",
+        description: "Stone columns rise from rocky shores at this Topton home on Lake Nantahala. Decks with living and dining areas, a barbecue, and a hot tub look out to lake and mountains, and steps lead to the private dock.",
         price: 1660.00
       },
       {
@@ -81,7 +81,7 @@ module.exports = {
         lat: -40.7622,
         lng: -2.4220,
         name: "The Watershed Cabin",
-        description: "Nestled between an abundance of leafy trees, this North Carolina escape boasts complete privacy. The 7,500-square-foot home sits at the edge of a peninsula on Fontana Lake with rippling waters below and soaring mountain peaks above. Grab a bottle of wine to sip on the covered porch — perhaps while soaking in the salt-water hot tub. Billiards and a cinema room keep the entertainment going at home.",
+        description: "Nestled between an abundance of leafy trees, this North Carolina escape boasts complete privacy. The 7,500-square-foot home sits at the edge of a peninsula on Fontana Lake with rippling waters below and soaring mountain peaks above.",
         price: 3250.00
       },
       {
@@ -93,7 +93,7 @@ module.exports = {
         lat: -40.2245,
         lng: -2.2430,
         name: "Shore Beats Work",
-        description: "Perfect for larger groups, this breezy island home in the Isle of Palms community has accommodations for 12 and plenty of scenic space for everyone to find privacy when needed. From the rooftop balcony, you’ll enjoy sweeping ocean views. For a closer look, follow the beach pathway to the shore for a swim. Later, get some rest in a poolside lounge chair or lay back in the hot tub.",
+        description: "Perfect for larger groups, this breezy island home in the Isle of Palms community has accommodations for 12 and plenty of scenic space for everyone to find privacy when needed.",
         price: 1470.00
       },
       {
@@ -117,7 +117,7 @@ module.exports = {
         lat: 42.7645,
         lng: 11.4730,
         name: "Architectural Wonder Above Sunset-WeHo w/ Big View",
-        description: "Midcentury modern 2bed/2bath stilt home with spectacular views above Sunset Strip (2 blocks up from Hollywood + Fairfax). Only blocks from the action, but very private and quiet. Recent renovations from roof to foundation, heat/AC system, 1 Giga/sec wifi, wired in + out with 11 speakers, movie projector + two 4k TVs (free Netflix, HBOMax and AppleTV+), 2-car parking with level 2 electric charger.",
+        description: "Midcentury modern 2bed/2bath stilt home with spectacular views above Sunset Strip (2 blocks up from Hollywood + Fairfax). Only blocks from the action, but very private and quiet.",
         price: 345.00
       },
       {
@@ -129,7 +129,7 @@ module.exports = {
         lat: 40.7245,
         lng: 12.4730,
         name: "Chateau de Banks",
-        description: "Just off North Beverly Hills Drive, this classical French style estate is in prime location for enjoying all this iconic area has to offer. With an incredible amount of indoor and outdoor space, it's perfect for large scale entertaining, extended family vacations, and maybe even a romantic wedding ceremony.",
+        description: "Just off North Beverly Hills Drive, this classical French style estate is in prime location for enjoying all this iconic area has to offer.",
         price: 16264.00
       },
       {
@@ -141,7 +141,7 @@ module.exports = {
         lat: 1.7645,
         lng: 27.4730,
         name: "Million Dollar Views",
-        description: "Sierra Sunset Estate enjoys the most ideal location a lakefront home on Lake Tahoe can boast. Less than 10 minutes to Stateline, Nevada, the opportunities for entertainment are endless. If gaming brings you joy, pop into Harrah's Lake Tahoe, Montbleu Resort or Hard Rock Hotel and Casino for an array of gambling options. These establishments also offer a variety of venues for concerts, shows, spa delights and shopping. World class skiing is just up the hill, literally, with Heavenly Ski Resort the proud mountain of 4,800 skiable acres (the most in Lake Tahoe).",
+        description: "Sierra Sunset Estate enjoys the most ideal location a lakefront home on Lake Tahoe can boast. Less than 10 minutes to Stateline, Nevada, the opportunities for entertainment are endless.",
         price: 28000.00
       },
       {
@@ -153,7 +153,7 @@ module.exports = {
         lat: 62.7645,
         lng: 2.4730,
         name: "Sonoma Coast•Arch•Estate•Dramatic Ocean VUs",
-        description: "Sonoma Coast is a Frank Lloyd Wright inspired modern California Ranch w/ subtle Prairie & International architectural design; perched on a promontory overlooking cove w/ jaw dropping ocean views and mesmerizing - audible crashing white water waves. Complete with spa, BBQ, dry/wet sauna, chefs kitchen, lux bedding and linens, Le Creuset crockery, Mauviel stainless steel cookware, Viking appliances, Wolf small kitchen appliances, Jura Espresso Machine with frother, WIFI, streaming Roku & Alexa.",
+        description: "Sonoma Coast is a Frank Lloyd Wright inspired modern California Ranch w/ subtle Prairie & International architectural design; perched on a promontory overlooking cove w/ jaw dropping ocean views and mesmerizing - audible crashing white water waves.",
         price: 1500.00
       },
     ])
