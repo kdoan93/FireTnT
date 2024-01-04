b0b721e48fd40550f7317eff87baa005eada1138
Kenny Doan <kdoan93@gmail.com>
2024-01-04 17:43:08 -0500

    Added review seeders

--- backend/db/seeders/20230810152734-demo-review.js
+++ backend/db/seeders/20230810152734-demo-review.js
@@ -45,6 +45,54 @@ module.exports = {
         review: 'It was aight... I guess.',
         stars: 4
       },
+      {
+        spotId: 5,
+        userId: 2,
+        review: 'Lake views were amazing. Sun burns were not.',
+        stars: 3
+      },
+      {
+        spotId: 6,
+        userId: 1,
+        review: 'Best lake views in North Cackalacky!',
+        stars: 5
+      },
+      {
+        spotId: 7,
+        userId: 4,
+        review: 'Fancy stay, fancier price',
+        stars: 4
+      },
+      {
+        spotId: 8,
+        userId: 1,
+        review: 'Great location surrounded by pizza shops!',
+        stars: 5
+      },
+      {
+        spotId: 9,
+        userId: 4,
+        review: 'Lights from LA were too bright',
+        stars: 3
+      },
+      {
+        spotId: 10,
+        userId: 1,
+        review: 'Amazing stay made me want to do the Carlton',
+        stars: 5
+      },
+      {
+        spotId: 11,
+        userId: 2,
+        review: 'Absolutely amazing mountain and lake views. Even more amazing price',
+        stars: 4
+      },
+      {
+        spotId: 8,
+        userId: 3,
+        review: 'Gained too much weight eating pizza everyday 😭',
+        stars: 4
+      },
     ])
   },
 
