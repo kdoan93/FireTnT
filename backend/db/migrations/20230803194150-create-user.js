2916c20726542944e44011f1d0d0b02c701cf979
Kenny Doan <kdoan93@gmail.com>
2024-01-05 12:58:40 -0500

    Changed line 60 in user migrations

--- backend/db/migrations/20230803194150-create-user.js
+++ backend/db/migrations/20230803194150-create-user.js
@@ -57,6 +57,6 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     // await queryInterface.dropTable('Users');
     options.tableName = "Users";
-    return queryInterface.dropTable(options);
+    return queryInterface.dropTable('Users');
   }
 };
