0dbba688f4d35ace80e2150524e650b52fa2a8c1
Kenny Doan <kdoan93@gmail.com>
2024-01-08 11:38:27 -0500

    debug

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -16,16 +16,16 @@ let enhancer;
 // MAY BE ISSUE
 // LOOKING FOR REDUX LOGGER when in production
 
-// if (process.env.NODE_ENV === 'production') {
-//   enhancer = applyMiddleware(thunk);
-// } else {
-//   const logger = require('redux-logger').default;
-//   const composeEnhancers =
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-// }
+if (process.env.NODE_ENV == 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require('redux-logger').default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
 
-if (process.env.NODE_ENV === 'production') enhancer = applyMiddleware(thunk)
+// if (process.env.NODE_ENV === 'production') enhancer = applyMiddleware(thunk)
 
 const configureStore = (preloadedState) => {
     return createStore(rootReducer, preloadedState, enhancer);
