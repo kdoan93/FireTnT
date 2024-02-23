9c0e2f1040d551cbba09b47757e908618a38914e
Kenny Doan <kdoan93@gmail.com>
2024-02-23 13:40:15 -0500

    WIP Search bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import SearchBar from '../SearchBar';
 import './Navigation.css';
+import { getSpots } from '../../store/spots';
 
 const airbnbLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png'
 
 function Navigation({ isLoaded }){
+
+  const dispatch = useDispatch()
+
   const sessionUser = useSelector(state => state.session.user);
+
+  const spots = useSelector(state => state.spot.allSpots)
+
+  const allSpots = Object.values(spots)
+
+  useEffect(() => {
+    dispatch(getSpots())
+  }, [dispatch])
+
+  if(!allSpots.length) return null
 
   return (
       <div className='navigationBar'>
         <NavLink exact to="/" className='home-button' >
             <img className='logo' src={airbnbLogo} alt='logo' />
         </NavLink>
+
+        <SearchBar
+          placeholder={'Search destinations'}
+          data={allSpots}
+        />
+
         {isLoaded && (
           <ProfileButton className='profileButton' user={sessionUser} />
-
         )}
       </div>
   );
---
+++ frontend/src/components/SearchBar/index.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router'
+
+const SearchBar = ({ placeholder, data }) => {
+    const history = useHistory()
+    const [filteredData, setFilteredData] = useState([])
+    const [wordEntered, setWordEntered] = useState("")
+
+    const clearInput = () => {
+        setFilteredData([])
+        setWordEntered("")
+    }
+
+    const toSpot = (id) => {
+        history.push(`/spots/${id}`)
+        clearInput()
+    }
+
+    const handleResults = (e) => {
+        const searchWord = e.target.value.toLowerCase()
+        setWordEntered(searchWord)
+        const newData = data.filter((value) => {
+            return value.name.toLowerCase().includes(searchWord)
+        })
+        if (searchWord === "") {
+            setFilteredData([])
+        } else {
+            setFilteredData(newData)
+        }
+    }
+
+    return (
+        <div className='searchBar-container'>
+            <div className='searchBar-icon'>
+                <i className='fa-solid fa-magnifying-glass'/>
+            </div>
+            <input
+                type='text'
+                value={wordEntered}
+                className='searchBar'
+                placeholder={placeholder}
+                onChange={handleResults}
+            />
+            <div className='searchBar-icon'>
+                {wordEntered.length !== 0 && <i onClick={clearInput} id='search-clear-button' className='fa-solid fa-x'></i>}
+            </div>
+            {filteredData.length !== 0 &&
+                <div className='search-bar-results'>
+                    {filteredData.map((data) => {
+                        let price = data.price
+                        let templatePrice = []
+                        for (let i = 0; i < price; i++) {
+                            templatePrice.push('$')
+                        }
+
+                        return (
+                            <div className='search-bar-results' onClick={() => toSpot(data.id)}>
+                                <div className='search-bar-result-img-container'>
+                                    <img className='search-bar-result-img' src={data.previewImage}></img>
+                                </div>
+                                <div className='search-bar-result-info-container'>
+                                    <div className='search-bar-result-name-address'>
+                                        {data.name} ({data.city}, {data.state})
+                                    </div>
+                                </div>
+                            </div>
+
+                        )
+                    })}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default SearchBar
