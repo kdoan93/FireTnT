12317825c1d101c6da12cb00f01e5ef70ee05f0d
Kenny Doan <kdoan93@gmail.com>
2024-03-12 19:18:29 -0400

    WIP store for spotsImages

--- frontend/src/store/spotsImages.js
+++ frontend/src/store/spotsImages.js
@@ -27,3 +27,70 @@ const initialState = {}
 // const spotImageReducer = ( state = initialState, action) => {
 
 // }
+
+
+/*
+
+
+import { csrfFetch } from "./csrf";
+
+// TYPE_CONSTANTS
+const GET_ALL_SPOT_IMAGES = 'spots/GET_ALL_SPOT_IMAGES';
+const POST_SPOT_IMAGE = 'spots/POST_SPOT_IMAGE';
+
+// POJO action creator
+const getAllSpotImages = (image, spotId) => {
+    return { type: GET_ALL_SPOT_IMAGES, image, spotId }
+}
+
+const postAnImage = spotImage => {
+    return {
+        type: POST_SPOT_IMAGE,
+        spotImage
+    }
+};
+
+export const getSpotImages = (spotId) => async(dispatch) => {
+    const response = await csrfFetch(`/api/spots/${spotId}/images`)
+    if (response.ok) {
+        const spotImages = await response.json()
+        dispatch(getAllSpotImages(spotId))
+        return spotImages
+    } else {
+        const errors = await response.json()
+        return errors
+    }
+}
+
+// POST spotImage thunk action
+export const createSpotImage = (spotImage, spotId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/spots/${spotId}/images`, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(spotImage)
+    })
+    if (response.ok) {
+        const newImage = await response.json()
+        dispatch(postAnImage(newImage))
+        return newImage;
+    } else {
+        const errors = await response.json()
+        return errors
+    }
+}
+
+const initialState = { singleSpot: { SpotImages: [] } }
+
+const spotsImageReducer = ( state = initialState, action ) => {
+    let newState;
+    switch (action.type) {
+
+        case GET_ALL_SPOT_IMAGES:
+            newState = { ...state, singleSpot: {} }
+            // Create action state
+    }
+}
+
+
+
+*/
