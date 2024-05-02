81b1ce16a5518079e0aeaacc4b89ff89335a1718
Kenny Doan <kdoan93@gmail.com>
2024-05-02 19:55:12 -0400

    Updated react-dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,64 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+// BrowserRouter used for React to route
+import { BrowserRouter } from 'react-router-dom';
+import { ModalProvider, Modal } from "./context/Modal";
+import App from './App';
+// Configures Redux store
+import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+import * as spotsActions from './store/spots';
+import * as reviewsActions from './store/reviews';
+import './index.css';
+
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+const root = createRoot(container)
+// Provider used to provide the Redux store
+
+// store variable used to access Redux store
+const store = configureStore();
+
+// Exposes store to window ONLY in development
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+  window.spotsActions = spotsActions;
+  window.reviewsActions = reviewsActions;
+}
+
+// Wrap the application with the Modal provider and render the Modal component
+// after the App component so that all the Modal content will be layered as
+// HTML elements on top of the all the other HTML elements:
+function Root() {
+  return (
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
+  );
+}
+
+root.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
+
+
+/*
+import React from 'react';
+
 import './index.css';
 
 import ReactDOM from 'react-dom';
@@ -52,3 +111,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+*/
