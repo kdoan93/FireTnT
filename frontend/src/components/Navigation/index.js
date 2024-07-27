e87622c2613f3a2ab8b221d64f266093522e134c
Kenny Doan <kdoan93@gmail.com>
2024-07-26 20:39:52 -0400

    Centered and fixed NavLink over extending

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,6 +5,7 @@ import ProfileButton from './ProfileButton';
 import SearchBar from '../SearchBar';
 import './Navigation.css';
 import { getSpots } from '../../store/spots';
+import { isMobile } from 'react-device-detect';
 
 const airbnbLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png'
 
@@ -27,6 +28,7 @@ function Navigation({ isLoaded }){
   return (
       <div className='navigationBar'>
         <div className='upperNav'>
+          {isMobile && <div className='navigationMobileSpace'></div>}
           <NavLink exact to="/" className='home-button' >
               <img className='logo' src={airbnbLogo} alt='logo' />
           </NavLink>
