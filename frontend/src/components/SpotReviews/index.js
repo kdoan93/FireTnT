a5a04a7b829f9a421e49110b468d340b466a6832
Kenny Doan <kdoan93@gmail.com>
2024-09-20 18:23:05 -0400

    Resolved Spot.firstName bug

--- frontend/src/components/SpotReviews/index.js
+++ frontend/src/components/SpotReviews/index.js
@@ -5,6 +5,7 @@ import { getSpotReviews } from "../../store/reviews";
 import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
 import { DeleteReviewModal } from "../ReviewModal/DeleteReviewModal";
 import './SpotReviews.css'
+import { getUserBookings } from "../../store/bookings";
 
 const SpotReviews = () => {
 
@@ -17,6 +18,64 @@ const SpotReviews = () => {
 
     let reviews = useSelector(state => state.review.spot)
 
+    let bookings = useSelector(state => state.booking.booking)
+
+    console.log("bookings: ", bookings)
+
+    useEffect(() => {
+        dispatch(getSpotReviews(spotId))
+        dispatch(getUserBookings())
+    }, [dispatch, spotId])
+
+    if (!reviews) return null
+    if (!sessionUser) sessionUser = 0;
+    if (!bookings) return
+
+    function hasBooked() {
+        let bookingsArr = Object.values(bookings)
+        for (let booking of bookingsArr) {
+            if (compareDates(booking.endDate, new Date())) console.log(`booking end date < today, ${booking.endDate}`)
+        }
+    }
+
+    hasBooked()
+
+    function compareDates(endBooking, today) {
+        endBooking = endBooking.toString()
+        today = today.toString()
+
+        let bookingStr = ''
+        let todayStr = ''
+
+        let monthNums = {
+            'Jan' : '01',
+            'Feb' : '02',
+            'Mar' : '03',
+            'Apr' : '04',
+            'May' : '05',
+            'Jun' : '06',
+            'Jul' : '07',
+            'Aug' : '08',
+            'Sep' : '09',
+            'Oct' : '10',
+            'Nov' : '11',
+            'Dec' : '12'
+        }
+
+        let bookingMonth = endBooking.slice(5, 7)
+        let bookingDays = endBooking.slice(8, 10)
+        let bookingYear = endBooking.slice(0, 4)
+
+        let todayMonth = today.slice(4, 7)
+        let todayDays = today.slice(8, 10)
+        let todayYear = today.slice(11, 15)
+
+        bookingStr = bookingMonth + bookingDays + bookingYear
+        todayStr = monthNums[todayMonth] + todayDays + todayYear
+        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", 'booking ', bookingStr, 'today ', todayStr, bookingStr < todayStr)
+        return bookingStr < todayStr
+    }
+
     const spotReviews = Object.values(reviews).reverse()
 
     function lowBudgetDateConverter(date) {
@@ -25,15 +84,6 @@ const SpotReviews = () => {
         let year = newDate.substring(10, 16)
         return month.concat(year)
     }
-
-    useEffect(() => {
-        dispatch(getSpotReviews(spotId))
-    }, [dispatch, spotId])
-
-    if(!reviews) return null
-    if(!sessionUser) sessionUser = 0;
-
-    // console.log("reviews: ", reviews)
 
     return (
         <div className="spotReviewsContainer">
